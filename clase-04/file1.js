fe5ea1ac9674aa688a18cf07382865cb5af1e6b6
Elizabeth Martinez <marieli.martinezb@gmail.com>
2022-08-23 09:59:23 -0500

    Implementación de try catch

--- clase-04/file1.js
+++ clase-04/file1.js
@@ -3,5 +3,21 @@ const fs = require('fs')
 
 /* Función para escribir en modo sincrónico, no lleva callback */
 // Si el archivo no existe, lo crea
+
 // fs.writeFileSync('clase04.txt', 'Mi primera vez escribiendo en un archivo')
-fs.writeFileSync('clase04.txt', 'Mi segunda vez escribiendo en un archivo')
+// fs.writeFileSync('clase04.txt', 'Mi segunda vez escribiendo en un archivo')
+
+// Lee el archivo, no lleva callback, devuelve el contenido del archivo
+// Pasamos el segundo argumento, para que la salida esté codificada en utf-8
+// let contenido = fs.readFileSync('clase04.txt', 'utf-8')
+// console.log(contenido);
+
+/* Forma 2, usando toString */
+/* Implementamos un trycatch, en caso de que no exista el archivo que se va leer, captura el error */
+
+try {
+    let contenido = fs.readFileSync('clase04.txt')
+    console.log(contenido.toString());
+} catch  {
+    
+}
